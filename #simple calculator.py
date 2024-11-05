874542ade76684c704ae566cae4487609c002450
Amartyayandrapu <59309767+Amartyayandrapu@users.noreply.github.com>
2024-11-04 19:33:12 -0600

    Add files via upload

---
+++ #simple calculator.py
@@ -0,0 +1,18 @@
+#simple calculator
+# Get two numbers from the user
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+
+# Perform arithmetic operations
+add = a + b
+sub = a - b
+mult = a * b
+div = a / b 
+mod = a % b 
+
+# Show the results
+print(f"Addition: {add}")
+print(f"Subtraction: {sub}")
+print(f"Multiplication: {mult}")
+print(f"Division: {div}")
+print(f"Modulus: {mod}")
